7575228318322e3e7ed4733b0afed4749df3e7f8
thaophan <phthaocse@gmail.com>
2018-11-02 23:20:25 +0700

    add built-in func, fundecl, vardecl

--- upload/src/main/mp/checker/StaticCheck.py
+++ upload/src/main/mp/checker/StaticCheck.py
@@ -1,6 +1,6 @@
 
 """
- * @author nhphung
+ * @author Phan Thao
 """
 from AST import * 
 from Visitor import *
@@ -20,8 +20,17 @@ class Symbol:
 
 class StaticChecker(BaseVisitor,Utils):
 
-    global_envi = [Symbol("getInt",MType([],IntType())),
-    			   Symbol("putIntLn",MType([IntType()],VoidType()))]
+    global_envi =  [Symbol("getInt",MType([],IntType())),
+    			    Symbol("putInt",MType([IntType()],VoidType())),
+                    Symbol("putIntLn",MType([IntType()],VoidType())),
+                    Symbol("getFloat",MType([],FloatType())),
+                    Symbol("putFloat",MType([FloatType()],VoidType())),
+                    Symbol("putFloatLn",MType([FloatType()],VoidType())),
+                    Symbol("putBool",MType([BoolType()],VoidType())),
+                    Symbol("putBoolLn",MType([BoolType()],VoidType())),
+                    Symbol("putString",MType([StringType()],VoidType())),
+                    Symbol("putStringLn",MType([StringType()],VoidType())),
+                    Symbol("putLn",MType([],VoidType()))]
             
     
     def __init__(self,ast):
@@ -30,24 +39,44 @@ class StaticChecker(BaseVisitor,Utils):
     def check(self):
         return self.visit(self.ast,StaticChecker.global_envi)
 
+    def checkRedeclared(self,sym,kind,env):
+        if self.lookup(sym.name,env, lambda x:x.name):
+            raise Redeclared(Variable(),ast.variable.name)
+        else:
+            return []
+
     def visitProgram(self,ast, c): 
-        return [self.visit(x,c) for x in ast.decl]
+        return [self.visit(x,x+c) for x in ast.decl]
 
     def visitFuncDecl(self,ast, c): 
-        return list(map(lambda x: self.visit(x,(c,True)),ast.body)) 
+        tmp = list(map(lambda x: self.visit(x,(c,True)),ast.body)) 
+        kind = Procedure() if type(ast.returnType) is VoidType else Function()
+        return self.checkRedeclared(Symbol(ast.name.name,MType([x.varType for x in ast.param],ast.returnType)),kind,c)
     
+    def visitVarDecl(self,ast,c):
+        return self.checkRedeclared(Symbol(ast.variable.name,ast.varType),Variable(),c)   
 
     def visitCallStmt(self, ast, c): 
         at = [self.visit(x,(c[0],False)) for x in ast.param]
         
-        res = self.lookup(ast.method.name,c[0],lambda x: x.name)
+        res = self.lookup(ast.method.name,c[0],lambda x: x.name)#symbol
         if res is None or not type(res.mtype) is MType or not type(res.mtype.rettype) is VoidType:
             raise Undeclared(Procedure(),ast.method.name)
-        elif len(res.mtype.partype) != len(at):
+        elif len(res.mtype.partype) != len(at) or True in [type(a) != type(b) for a,b in zip(at,res.mtype.partype)]:
             raise TypeMismatchInStatement(ast)            
         else:
             return res.mtype.rettype
 
     def visitIntLiteral(self,ast, c): 
         return IntType()
+
+    def visitFloatLiteral(self,ast,c):
+        return FloatType()
+
+    def visitBooleanLiteral(self,ast,c):
+        return BoolType()
+
+    def visitStringLiteral(self,ast,c):
+        return StringType()    
+    
 
